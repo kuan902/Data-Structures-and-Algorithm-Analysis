ec0534fbf1a0a0595a9a1779e4badacf953ff30c
Kuan <1772017002@qq.com>
2026-08-17 04:18:55 +0000

    快慢指针2

---
+++ test2.cpp
@@ -0,0 +1,72 @@
+#include <iostream>
+using namespace std;
+
+typedef struct LNode {
+    char data;
+    struct LNode* next;
+} LNode, *LinkList;
+
+int getLength(LinkList L) {
+    int len = 0;
+    LinkList p = L->next;
+    while (p != nullptr) {
+        len++;
+        p = p->next;
+    }
+    return len;
+}
+
+LNode* findCommonSuffix(LinkList str1, LinkList str2) {
+    int len1 = getLength(str1);
+    int len2 = getLength(str2);
+    LinkList p = str1->next;
+    LinkList q = str2->next;
+
+    if (len1 > len2) {
+        int d = len1 - len2;
+        while (d--) p = p->next;
+    } else {
+        int d = len2 - len1;
+        while (d--) q = q->next;
+    }
+
+    while (p != nullptr && q != nullptr && p != q) {
+        p = p->next;
+        q = q->next;
+    }
+    return p;
+}
+
+int main() {
+    // ========= 手动构造原题真正共享结点的链表 =========
+    // 公共后缀结点 i -> n -> g
+    LNode* i_node = new LNode{'i', nullptr};
+    LNode* n_node = new LNode{'n', nullptr};
+    LNode* g_node = new LNode{'g', nullptr};
+    i_node->next = n_node;
+    n_node->next = g_node;
+
+    // str1: head -> l -> o -> a -> d -> i_node
+    LinkList str1 = new LNode;
+    str1->next = new LNode{'l',
+        new LNode{'o',
+            new LNode{'a',
+                new LNode{'d', i_node}
+            }
+        }
+    };
+
+    // str2: head -> b -> e -> i_node
+    LinkList str2 = new LNode;
+    str2->next = new LNode{'b',
+        new LNode{'e', i_node}
+    };
+
+    LNode* res = findCommonSuffix(str1, str2);
+    if (res != nullptr) {
+        cout << "共同后缀起始结点data = " << res->data << endl;
+    } else {
+        cout << "无公共后缀" << endl;
+    }
+    return 0;
+}
